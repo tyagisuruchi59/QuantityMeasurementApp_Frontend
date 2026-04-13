cbee66df55b7f1a0916c5149a3400d3b6125ff96
tyagisuruchi59 <suruchityagi972@gmail.com>
2026-04-13 15:21:25 +0530

    Fix API URL to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5167/api/v1',
+  baseURL: 'https://quantitymeasurementapp-pz1j.onrender.com/api/v1',
   headers: { 'Content-Type': 'application/json' }
 });
 
